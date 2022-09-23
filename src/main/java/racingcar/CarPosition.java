5ccd02e2c5544a0c1f52b0426da518bfc479d722
by-gramm <ready2start@naver.com>
2022-09-23 18:11:18 +0900

    feat: Add CarPosition creation and exception handling

---
+++ src/main/java/racingcar/CarPosition.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+public class CarPosition {
+
+    private final int position;
+
+    private CarPosition(int position) {
+        this.position = position;
+    }
+
+    public static CarPosition of(int position) {
+        if (position < 0) {
+            throw new IllegalArgumentException("자동차 위치는 0 이상의 정수만 가능합니다.");
+        }
+
+        return new CarPosition(position);
+    }
+}
---
+++ src/test/java/racingcar/CarPositionTest.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CarPositionTest {
+
+    @Test
+    void 자동차_위치_0이상의_정수() {
+        assertThatCode(() -> CarPosition.of(0))
+                .doesNotThrowAnyException();
+
+        assertThatThrownBy(() -> CarPosition.of(-1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 위치는 0 이상의 정수만 가능합니다.");
+    }
+
+    @Test
+    void 자동차_위치_생성() {
+        assertThat(CarPosition.of(3))
+                .isInstanceOf(CarPosition.class);
+    }
+}
