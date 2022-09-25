1877363de0fe1365782de1856a7028607fe479cc
by-gramm <ready2start@naver.com>
2022-09-25 19:46:01 +0900

    feat: Add random number creation

--- src/main/java/racingcar/domain/RandomNumber.java
+++ src/main/java/racingcar/domain/RandomNumber.java
@@ -1,11 +1,22 @@
 package racingcar.domain;
 
+import java.util.Random;
+
 public class RandomNumber {
+
+    public static final int MAX_NUMBER_BOUND = 10;
 
     private final int number;
 
     private RandomNumber(int number) {
         this.number = number;
+    }
+
+    public static RandomNumber of() {
+        Random random = new Random();
+        int number = random.nextInt(MAX_NUMBER_BOUND);
+
+        return new RandomNumber(number);
     }
 
     public static RandomNumber of(int number) {
